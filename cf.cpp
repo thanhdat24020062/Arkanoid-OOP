fa1391abd86c78bb6cfb38da3c2b22831558e692
Nguyễn Hữu Thành Đạt <thanhdat2105nk@gmail.com>
2025-09-26 21:24:45 +0700

    Create cf.cpp

---
+++ cf.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"dat dep trai nhat the gioi"<<'\n';
+    int n;
+    cin>>n;
+    for (int i=1;i<=n;i++) cout<<i<<" ";
+    return 0;
+}
