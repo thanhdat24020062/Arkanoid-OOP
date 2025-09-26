7e71e78857669d0fcc23a62dde815ebb79501cca
lexuanbac-06 <1le1bac1@gmail.com>
2025-09-26 22:00:15 +0700

    đạt 09 hihi

--- cf.cpp
+++ cf.cpp
@@ -1,8 +1,8 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
 int main(){
-    cout<<"dat dep trai nhat the gioi"<<'\n';
+    cout<<"dat 09"<<'\n';
     int n;
     cin>>n;
     for (int i=1;i<=n;i++) cout<<i<<" ";
