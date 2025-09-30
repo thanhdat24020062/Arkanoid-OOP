9f22101be66436da7273bfa9bbaa736d28ede159
lexuanbac-06 <1le1bac1@gmail.com>
2025-09-30 11:04:42 +0700

    thêm giao diện đăng nhập

--- bac_ronando.java
+++
@@ -1,9 +0,0 @@
-
-public class bac_ronando {
-    public static void siuu( ){
-
-public class dat_pepsi {
-    public static void siu(){
-        System.out.println("siuu :0");
-    }
-}
--- main.java
+++ main.java
@@ -1,6 +1,14 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("helo arkanoid!");
-        bac_ronando.siuu();
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ignored) {}
+
+        javax.swing.SwingUtilities.invokeLater(Login::new);
     }
-} 
+}
