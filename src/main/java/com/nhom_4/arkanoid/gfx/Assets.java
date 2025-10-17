044e756c88291ebea9bb7c102837757def559027
lexuanbac-06 <1le1bac1@gmail.com>
2025-10-17 22:05:53 +0700

    chỉnh đường dẫn file

--- src/main/java/com/nhom_4/arkanoid/gfx/Assets.java
+++ src/main/java/com/nhom_4/arkanoid/gfx/Assets.java
@@ -25,27 +25,25 @@ public class Assets {
     public static Image ball;
     public static List<Image> bricks = new ArrayList<>();
     public static void load() {
-        MENU_BG = Files.loadImageCP("/resources/images/menu_bg.png", Constants.WIDTH, Constants.HEIGHT);
-        BUTTON_START = Files.loadImageCP("/resources/images/button_menu.png", 240, 120);
-        BUTTON_HOW_TO_PLAY = Files.loadImageCP("/resources/images/button_menu.png", 460, 120);
-        BUTTON_EXIT = Files.loadImageCP("/resources/images/button_menu.png", 200, 120);
-        BUTTON_SMALL=Files.loadImageCP("/resources/images/button_menu.png", 150, 90);
-        EXIT_DIALOG=Files.loadImageCP("/resources/images/exit.png", 900, 600);
+        MENU_BG = Files.loadImageCP("/images/menu_bg.png", Constants.WIDTH, Constants.HEIGHT);
+        BUTTON_START = Files.loadImageCP("/images/button_menu.png", 240, 120);
+        BUTTON_HOW_TO_PLAY = Files.loadImageCP("/images/button_menu.png", 460, 120);
+        BUTTON_EXIT = Files.loadImageCP("/images/button_menu.png", 200, 120);
+        BUTTON_SMALL=Files.loadImageCP("/images/button_menu.png", 150, 90);
+        EXIT_DIALOG=Files.loadImageCP("/images/exit.png", 900, 600);
         //load font chữ
-        fontPixels_40 = Files.loadFont("/resources/fonts/font1.ttf", 0, 40);
-        fontPixels_44 = Files.loadFont("/resources/fonts/font1.ttf", 0, 44);
+        fontPixels_40 = Files.loadFont("/fonts/font1.ttf", 0, 40);
+        fontPixels_44 = Files.loadFont("/fonts/font1.ttf", 0, 44);
    
         // Cung cấp đường dẫn tương đối từ thư mục gốc của dự án
         // background = loadImage("src/main/resources/images/background.png");
-        paddle = Files.loadImage("src/main/resources/images/paddle.png");
-        ball = Files.loadImage("src/main/resources/images/ball.png");
+        paddle = Files.loadImage("/images/paddle.png");
+        ball = Files.loadImage("/images/ball.png");
 
-        bricks.add(Files.loadImage("src/main/resources/images/brick_gold.png"));
-        bricks.add(Files.loadImage("src/main/resources/images/brick_orange.png"));
-        bricks.add(Files.loadImage("src/main/resources/images/brick_silver_2.png"));
-        bricks.add(Files.loadImage("src/main/resources/images/brick_blue.png"));
-        bricks.add(Files.loadImage("src/main/resources/images/brick_cyan.png"));
-
-        System.out.println("Tải tài nguyên thành công!");
+        bricks.add(Files.loadImage("/images/brick_gold.png"));
+        bricks.add(Files.loadImage("/images/brick_orange.png"));
+        bricks.add(Files.loadImage("/images/brick_silver_2.png"));
+        bricks.add(Files.loadImage("/images/brick_blue.png"));
+        bricks.add(Files.loadImage("/images/brick_cyan.png"));
     }
 }
--- src/main/java/com/nhom_4/arkanoid/util/Files.java
+++ src/main/java/com/nhom_4/arkanoid/util/Files.java
@@ -1,6 +1,7 @@
 package com.nhom_4.arkanoid.util;
 
 import javax.imageio.ImageIO;
+import javax.print.DocFlavor.URL;
 import javax.swing.ImageIcon;
 
 import com.nhom_4.arkanoid.gfx.Assets;
@@ -39,15 +40,37 @@ public class Files {
             return null;
         }
     }
-
-    public static Image loadImage(String path) {
-        File f = new File(path);
-        if (!f.exists()) {
-            System.err.println("LỖI NGHIÊM TRỌNG: File không tồn tại tại đường dẫn: " + f.getAbsolutePath());
-            System.exit(1);
+public static Image loadImage(String pathOrCp) {
+    try {
+        // 1) Thử classpath (khi bạn truyền "/images/menu_bg.png")
+        //    => yêu cầu ảnh nằm ở src/main/resources/images/...
+        java.net.URL url = Files.class.getResource(pathOrCp);
+        if (url != null) {
+            // Dùng ImageIO để load đồng bộ, tránh ảnh “chưa kịp decode”
+            BufferedImage bi = ImageIO.read(url);
+            if (bi != null) return bi;
         }
-        return new ImageIcon(f.getAbsolutePath()).getImage();
+
+        // 2) Fallback: thử đường dẫn file hệ thống (vd: "src/main/resources/images/menu_bg.png")
+        File f = new File(pathOrCp);
+        if (f.exists()) {
+            BufferedImage bi = ImageIO.read(f);
+            if (bi != null) return bi;
+        }
+
+        // 3) Báo lỗi rõ ràng
+        String fsAbs = f.getAbsolutePath();
+        throw new IllegalArgumentException(
+            "Không tìm thấy ảnh. Classpath='" + pathOrCp + "', FileSystem='" + fsAbs + "'"
+        );
+
+    } catch (Exception e) {
+        // In stacktrace để debug thật sự lỗi gì (sai path, ảnh hỏng, v.v.)
+        e.printStackTrace();
+        return null;
     }
+}
+
 
     public static Font loadFont(String cpPath, int style, float size) {
         try (InputStream is = Assets.class.getResourceAsStream(cpPath)) {
